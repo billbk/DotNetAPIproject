f91961696f691ca72ae339bec19247c095e9faff
bilbk311 <bilbk311@gmail.com>
2025-10-21 01:14:06 -0400

    Commit 2nd

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:poartnumber/api/Students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GEt:/https://localhost:poartnumber/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "john", "Jane", "Mark", "Emily", "David" };
+
+            return Ok(studentNames);
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInkm { get; set; }
+        public string? walkImageurl { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+        // Navigation properties
+        public Difficulty Difficulty { get; set; }
+        public Difficulty Region { get; set; }
+    }
+}
